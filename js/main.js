1c2e36de43213b4f35b4bff688b9646181e2298a
Zarzuket <davide.bajo@gmail.com>
2021-09-17 15:54:27 +0200

    scaffodling exercise

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+      message: "",
+    },
+    methods: "",
+  })
+
+
+
+// https://static-www.quotidianopiemontese.it/wp-content/2021/07/Italia-Belgio-Euro-2020-1.jpg
+// https://www.adnkronos.com/resources/026b-12b19793c0e6-a0ee2d5204de-1000/format/big/italia_spagna_06_afp.jpg
+// https://static.sky.it/images/skysport/it/calcio/europei/2021/07/10/italia-inghilterra-three-lions-no-della-uefa/tifosi_inglesi.jpg
+// https://staticfanpage.akamaized.net/wp-content/uploads/sites/27/2021/07/GettyImages-1327321430.jpg
+// https://images2.corriereobjects.it/methode_image/2021/07/11/Sport/Foto-Sport-Trattate/e1658b9afb79439dbf0c1818d38965a9-0113-kku-U32702002900443QBI-526x284@Corriere-Web-Sezioni.jpg
